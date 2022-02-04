ccea3584b9c221530dcbae8f74e55fb7153d3d9d
KronosGR <geo.elgeo@gmail.com>
2022-02-04 09:59:19 +0100

    added stupid countdown

--- script.js
+++ script.js
@@ -36,13 +36,13 @@ const wrongFormat = [];
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  // const correctEquations =
   // Set amount of wrong equations
-  // const wrongEquations = 
+  // const wrongEquations =
   // Loop through, multiply random numbers up to 9, push to array
   // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
   //   equationObject = { value: equation, evaluated: 'true' };
@@ -50,13 +50,13 @@ function createEquations() {
   // }
   // Loop through, mess with the equation results, push to array
   // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
   //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
   //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
+  //   const formatChoice =
   //   const equation = wrongFormat[formatChoice];
   //   equationObject = { value: equation, evaluated: 'false' };
   //   equationsArray.push(equationObject);
@@ -84,29 +84,49 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
-function getRadioValue(){
+function countdownStart(){
+  countdown.textContent = '3';
+  setTimeout(() => {
+    countdown.textContent = '2'
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = '1'
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = 'GO!'
+  }, 3000);
+}
+
+function showCountDown() {
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart();
+}
+
+function getRadioValue() {
   let radioValue;
-  radioInputs.forEach((radioInput)=> {
-    if (radioInput.checked){
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
       radioValue = radioInput.value;
     }
-  })
-  return radioValue
+  });
+  return radioValue;
 }
 
-function selectQuestionAmount(e){
+function selectQuestionAmount(e) {
   e.preventDefault();
-  questionAmount = getRadioValue()
-  console.log(questionAmount)
+  questionAmount = getRadioValue();
+
+  if (questionAmount) showCountDown();
 }
 
-startForm.addEventListener('click' , ()=> {
-  radioContainers.forEach((radioEl)=> {
+startForm.addEventListener('click', () => {
+  radioContainers.forEach((radioEl) => {
     radioEl.classList.remove('selected-label');
-    if (radioEl.children[1].checked){
-      radioEl.classList.add('selected-label')
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
     }
-  })
-})
+  });
+});
 
-startForm.addEventListener('submit', selectQuestionAmount)
+startForm.addEventListener('submit', selectQuestionAmount);
