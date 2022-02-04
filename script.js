dfedfcef530092e7ebd682b78b11d7904f65240a
KronosGR <geo.elgeo@gmail.com>
2022-02-04 09:54:34 +0100

    radio selected

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const playAgainBtn = document.querySelector('.play-again');
 
 // Equations
 
+let questionAmount = 0;
 let equationsArray = [];
 
 // Game Page
@@ -82,3 +83,30 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+function getRadioValue(){
+  let radioValue;
+  radioInputs.forEach((radioInput)=> {
+    if (radioInput.checked){
+      radioValue = radioInput.value;
+    }
+  })
+  return radioValue
+}
+
+function selectQuestionAmount(e){
+  e.preventDefault();
+  questionAmount = getRadioValue()
+  console.log(questionAmount)
+}
+
+startForm.addEventListener('click' , ()=> {
+  radioContainers.forEach((radioEl)=> {
+    radioEl.classList.remove('selected-label');
+    if (radioEl.children[1].checked){
+      radioEl.classList.add('selected-label')
+    }
+  })
+})
+
+startForm.addEventListener('submit', selectQuestionAmount)
